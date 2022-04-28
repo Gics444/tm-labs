d851b612f4b3e14323d0fd2fb283f2e0e04fbb39
BoneHairReptile <bonehairreptile@gmail.com>
2022-04-28 21:51:24 +0300

    V_1 2022_04_28
    - Added some basic entities and structures;
    - Added player;
    - Added plombir;

---
+++ Lab3/Godot stuff/Objects/Entities/Circle_(Player)/Circle_(Player).gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var movespeed = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	var motion = Vector2()
+	
+	if Input.is_action_pressed("up"):
+		motion.y -= 1
+	if Input.is_action_pressed("down"):
+		motion.y += 1
+	if Input.is_action_pressed("left"):
+		motion.x -= 1
+	if Input.is_action_pressed("right"):
+		motion.x += 1
+	
+	motion = motion.normalized()
+	motion = move_and_slide(motion * movespeed)
---
+++ Lab3/Godot stuff/Objects/Structures/Pest_Deepnest/Sprite.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+export(Array, Texture) var TEXTURE_VARIATION_ARRAY: Array =[
+	preload("res://Objects/Structures/Pest_Deepnest/Pest_deepnest_1.png"),
+	preload("res://Objects/Structures/Pest_Deepnest/Pest_deepnest_2.png"),
+	preload("res://Objects/Structures/Pest_Deepnest/Pest_deepnest_3.png")
+]
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func variate_texture():
+	if TEXTURE_VARIATION_ARRAY.size() > 1:
+		var texture_id: int = randi() % TEXTURE_VARIATION_ARRAY.size()
+		texture = TEXTURE_VARIATION_ARRAY[texture_id]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	variate_texture(); # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
