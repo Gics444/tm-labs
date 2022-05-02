bd285bcaa837b226eaebc9819de05928b7e0f9d9
darkcat013 <viorel.noroc013@gmail.com>
2022-05-02 13:40:07 +0300

    added ui

---
+++ Lab3/Godot stuff/Main_Scene_(World)/ButtonGroup.gd
@@ -0,0 +1,18 @@
+extends Control
+
+onready var pressed = $Structure1
+
+func _ready() -> void:
+	for button in get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button])
+
+func _on_Button_pressed(button: Button) -> void:
+	pressed.pressed = false
+	pressed = button
+	pressed.pressed = true
+	
+func _input(event):
+	if event.is_action_pressed("1"):
+		$Structure1.emit_signal("pressed")
+	if event.is_action_pressed("2"):
+		$Structure2.emit_signal("pressed")
